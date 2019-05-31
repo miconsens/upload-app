c450ff05a98c02fdad139968b92c2304bd2ab685
Micaela <micaela@rt3347ml-ccm.sickkids.ca>
2019-05-31 12:54:14 -0400

    add routing :)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import SignUp from './SignUp';
-import Login from './Login';
+import Register from './Register';
+import Authenticate from './Authenticate';
+import HomePage from './HomePage';
 import App from './App';
 import './index.css';
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import * as R from 'ramda'
+
+const Router = () => {
+  const [pageKey, setPageKey] = useState('homePage')
+  const isPageKey = R.equals(pageKey)
+  // HOC for passing pageKey
+  // `withSetPageKey` is a function
+  const withSetPageKey = R.curry(
+    // ... that takes arguments `setPageKey` and `WrappedComponent`
+    (setPageKey, WrappedComponent) => (
+      // ... and returns another component with `setPageKey` passed as a prop to `WrappedComponent`
+      wrappedComponentProps => <WrappedComponent {...wrappedComponentProps} setPageKey={setPageKey} />
+    )
+  )(setPageKey)
+
+  return (
+    isPageKey('main') ? withSetPageKey(App)
+    : isPageKey('authenticate') ? withSetPageKey(Authenticate)
+    : isPageKey('register') ? withSetPageKey(Register)
+    : withSetPageKey(HomePage)
+  )()
+}
 
 ReactDOM.render(
-  <App />,
+  <Router/>,
   document.getElementById('root')
 );
-
