733829f32fbef5c20d06b6b4f74991aad6898530
Micaela <micaela@rt3347ml-ccm.sickkids.ca>
2019-06-04 12:20:57 -0400

    added apollo client

--- src/index.js
+++ src/index.js
@@ -5,13 +5,37 @@ import Authenticate from './Authenticate';
 import HomePage from './HomePage';
 import App from './App';
 import './index.css';
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider, Query,} from "react-apollo";
 import { ApolloClient } from 'apollo-client';
+import gql from 'graphql-tag';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
 import * as R from 'ramda'
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:8000/'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
+
+const USER_QUERY = gql`
+  {
+    uploads (userID:"5ced58bc6ac1cfab2a84b89a"){
+      filename
+    }
+  }
+`;
+
+//Running our query outside of React
+client.query({
+  query: USER_QUERY
+}).then(res => console.log(res))
 
 const Router = () => {
   const [pageKey, setPageKey] = useState('homePage')
@@ -35,6 +59,8 @@ const Router = () => {
 }
 
 ReactDOM.render(
-  <Router/>,
+  <ApolloProvider client={client}>
+  <Router/>
+  </ApolloProvider>,
   document.getElementById('root')
 );
