192312ffbf7977f238a5b91ce159e8fe519f32bb
Micaela <micaela@rt3347ml-ccm.sickkids.ca>
2019-06-04 14:53:39 -0400

    user uploads query

--- src/App.js
+++ src/App.js
@@ -5,10 +5,52 @@ import {Button, Segment, Header, Icon, Grid, Divider } from 'semantic-ui-react'
 
 import * as R from 'ramda'
 
+import {Query} from 'react-apollo'
 
-const App = ({setPageKey}) => {
+import gql from 'graphql-tag'
+
+const USER_UPLOADS = gql`{
+    user (userID:"5cf69f0c3d57a51cba9aece3"){
+        username
+        uploads{
+            filename
+            objectName
+            bucketName
+        }
+    }   
+}
+`
+
+const UploadsList = ({
+    user
+}) => (
+    <Query query={USER_UPLOADS} skip={R.isNil(user)}>
+    {
+        ({data: {user}, error, loading}) => (
+            !R.isNil(user) &&
+            R.compose(
+                R.map(
+                    ({objectName, filename}) => (
+                        <Segment clearing key={objectName}>
+                            {filename}
+                            <Button style={{background:'#82E0AA', color:'#ffff'}} floated='right' content='Download'
+                                as='a'
+                                target="_blank"
+                                download
+                                href={`http://localhost:4000/upload/download/${objectName}`}
+                            />
+                        </Segment>
+                    )
+                ),            
+                R.prop('uploads')
+            )(user)
+        )
+    }
+    </Query>
+)
+
+const App = ({setPageKey, user}) => {
     const [files, setFiles] = useState([])
-    console.log(files);
     useEffect(
          () => {
             const fetchUploads = async () => {
@@ -22,7 +64,14 @@ const App = ({setPageKey}) => {
     )
     return (
         <div style={{background: '#82E0AA'}}>
-        <Button size='medium' inverted floated='right' content='LOG-OUT' onClick={() => setPageKey('homePage')}/>
+        <Button
+            size='medium' inverted floated='right'
+            animated='fade'
+            onClick={() => setPageKey('homePage')}
+        >
+            <Button.Content content='LOG-OUT' hidden/>
+            <Button.Content content={`${R.prop('username', user)}... do you want to leave?`} visible/>
+        </Button>
         <Segment  attached='bottom' size='massive' placeholder>
             <Grid columns={2} style={{ height: '90vh' }} stackable textAlign='center'>
                 <Divider hidden vertical centered='true'>
@@ -52,7 +101,8 @@ const App = ({setPageKey}) => {
                             <Icon style={{color:'#82E0AA'}} name='circle' />
                             <br></br>
                             LIST OF UPLOADED FILES
-                            {R.map(
+                            <UploadsList user={user} />
+                            {/* {R.map(
                                 ({objectName, filename}) => (
                                     <Segment clearing key={objectName}>
                                         {filename}
@@ -63,21 +113,9 @@ const App = ({setPageKey}) => {
                                             href={`http://localhost:4000/upload/download/${objectName}`}
                                         />
                                     </Segment>
-                                    // <Segment key={filename} as={Button}
-                                    //     onClick={
-                                    //         () => {
-                                    //             console.log('test')
-                                    //             const xhr = new XMLHttpRequest ()
-                                    //             xhr.open('GET', `http://localhost:4000/upload/download/${objectName}`, true)
-                                    //             xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
-                                    //             xhr.send()
-                                    //         }
-                                    //     }
-                                    //     content={filename}
-                                    // />
                                 ),
                                 files
-                            )}
+                            )} */}
                         </Header>
                     </Grid.Column>
                 </Grid.Row>
