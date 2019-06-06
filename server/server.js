b764de1200171caa8933f994429cb4fa8a7c0ba4
Micaela <micaela@rt3347ml-ccm.sickkids.ca>
2019-06-06 15:37:46 -0400

    remove put HTTP request in favour of graphql

--- server/server.js
+++ server/server.js
@@ -11,52 +11,6 @@ const port = 4000
 const upload = multer({dest: '/Users/micaela/Desktop/my-app/server/tmp'})
 const R= require('ramda')
 
-// const resolvers = {
-//   User: {
-//     uploads: async ({userID}, variables, {Upload}) => {
-//       // console.log(userID)
-//       return await Upload.find({bucketName: userID})
-//      // return filter(uploads, { user: user.name });
-//     },
-//   },
-//   Query: {
-//     user: async (parent, {userID}, {User}) => {
-//       return await User.findOne({userID})
-//       //return context.Uploads.find(user, { id: args.id });
-//     },
-//     uploads: async (parent, {userID}, {Upload}) => {
-//       return await Upload.find({bucketName: userID})
-//       //return context.Users.find(uploads)
-//     }
-//   },
-//   Mutation: {
-//     authenticate: async (parent, {username, password}, {User}) =>{
-//       // console.log('auth', username, password)
-//       const returningUser = await User.findOne({username, password})
-//       if (!returningUser){
-//         throw new ApolloError('Username or password is invalid')
-//       }
-//       return returningUser
-//     },
-//     register: async (parent, {username, password}, {User}) =>{
-//       const existing =  await User.findOne({username});
-//       if (!existing){
-//         const newUser = new User({ username, password});
-//         // console.log('New user');
-//         const {userID} = await newUser.save()
-//         // Make a bucket with the user's ID.
-//         minioClient.makeBucket(`${userID}`, 'us-east-1', function(err) {
-//           if (err) return console.log(err)
-//           console.log('Users bucket created successfully in "us-east-1".')
-//         })
-//         return newUser
-//       } else {
-//         throw new ApolloError('User already exists')
-//       }
-//     }
-//   },
-// };
-
 const context = async ({req}) =>{
   return{
     req,
@@ -85,15 +39,6 @@ const serverOptions = {
   subscriptions: '/graphql'
 }
 
-// const minioClient = new minio.Client({
-//   endPoint: 'localhost',
-//   port: 9000,
-//   useSSL: false,
-//   // CHANGE THESE WHEN RUNNING MINIO FROM DOCKER
-//    accessKey: 'AKIAIOSFODNN7EXAMPLE',
-//    secretKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
-// });
-
 minioClient.listBuckets(function(e, buckets) {
   if (e) return console.log(e)
   console.log('buckets :', buckets)
@@ -109,21 +54,21 @@ objectsStream.on('error', function(e) {
 
 
 
-const fileNotUploaded = (file, allUploads) => (
-    R.not(R.includes(
-        R.prop('originalname', file),
-        R.pluck('filename', allUploads)
-    ))
-)
+// const fileNotUploaded = (file, allUploads) => (
+//     R.not(R.includes(
+//         R.prop('originalname', file),
+//         R.pluck('filename', allUploads)
+//     ))
+// )
 
-app.get('/upload/all',
-    async (req, res) => {
-        const uploads = await Upload.find({})
-        // console.log(uploads)
-        res.json(uploads)
-        //res.sendStatus(200)
-    }
-)
+// app.get('/upload/all',
+//     async (req, res) => {
+//         const uploads = await Upload.find({})
+//         // console.log(uploads)
+//         res.json(uploads)
+//         //res.sendStatus(200)
+//     }
+// )
 
 app.get('/upload/download/:objectName',
     async (req, res, next) => {
@@ -143,30 +88,31 @@ app.get('/upload/download/:objectName',
 })
 
 
-app.put('/upload', upload.single('uploadedFile'),
-    async (req, res, next) => {
-        const {file} = req
-        const {originalname} = file;
-        const upload = new Upload({bucketName: '5cf69f0c3d57a51cba9aece3', filename: originalname})
-        const allUploads = await Upload.find({})
-        // fileNotUploaded needs to be refactored to account for the userID bucketName
-        if (fileNotUploaded(file, allUploads)) {
-            const {bucketName, objectName, filename} = await upload.save()
-            console.log('UPLOADING FILE', bucketName, objectName)
-            const metaData = { 
-                'Content-Type': 'application/octet-stream',
-                'X-Amz-Meta-Testing': 1234,
-                'example': 5678
-            }
-            // Using fPutObject API upload your file to the bucket
-            minioClient.fPutObject(`${bucketName}`, `${objectName}`, file.path, metaData, function(err, etag) {
-                if (err) return console.log(err, etag)
-                console.log('File uploaded successfully.')
-            })
-        }
-        res.sendStatus(200);
-    }
-)
+// app.put('/upload', upload.single('uploadedFile'),
+//     async (req, res, next) => {
+//         const {file} = req
+//         const {originalname} = file;
+//         const upload = new Upload({bucketName: '5cf69f0c3d57a51cba9aece3', filename: originalname})
+//         const allUploads = await Upload.find({})
+//         // fileNotUploaded needs to be refactored to account for the userID bucketName
+//         if (fileNotUploaded(file, allUploads)) {
+//             const {bucketName, objectName, filename} = await upload.save()
+//             console.log('UPLOADING FILE', bucketName, objectName)
+//             const metaData = { 
+//                 'Content-Type': 'application/octet-stream',
+//                 'X-Amz-Meta-Testing': 1234,
+//                 'example': 5678
+//             }
+//             console.log(file.path, 'filePath')
+//             // Using fPutObject API upload your file to the bucket
+//             minioClient.fPutObject(`${bucketName}`, `${objectName}`, file.path, metaData, function(err, etag) {
+//                 if (err) return console.log(err, etag)
+//                 console.log('File uploaded successfully.')
+//             })
+//         }
+//         res.sendStatus(200);
+//     }
+// )
 db.once(
     'open',
     () => {
