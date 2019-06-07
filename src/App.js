b38cf8a152d249ac44f0213b6713c7751f677f6e
Micaela <micaela@rt3347ml-ccm.sickkids.ca>
2019-06-07 10:51:48 -0400

    createPresignedLink and change layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import {Button, Segment, Header, Icon, Grid, Divider } from 'semantic-ui-react'
 import Dropzone from 'react-dropzone'
@@ -36,132 +36,111 @@ const CREATE_UPLOAD_MUTATION = gql`
   }
 `;
 
+const CREATE_PRESIGNED_LINK_MUTATION = gql`
+  mutation createPresignedLink(
+      $bucketName: ID
+      $objectName: ID
+      ){
+      createPresignedLink(
+        bucketName: $bucketName
+        objectName : $objectName
+      ) 
+  }
+`
+
+const DownloadButton = ({
+    bucketName, objectName, filename,
+}) => (
+    <Mutation mutation={CREATE_PRESIGNED_LINK_MUTATION}>
+    {
+        (createPresignedLink, {called, data, error, loading}) => (
+            !console.log(data) &&
+            R.isNil(data) ?
+            <Button floated ='right' size='mini' content='Create Presigned Link' onClick={() => createPresignedLink({variables: {bucketName, objectName}})}/>
+            :
+            <Button style={{background:'#82E0AA', color:'#ffff'}} floated='right' content='Download'
+                as='a'
+                // target="_blank"
+                download={'test.mov'}
+                href={R.prop('createPresignedLink', data)}
+            />
+        )
+    }
+    </Mutation>
+)
+
 const UploadsList = ({
     user
 }) => (
     <Query query={USER_UPLOADS} skip={R.isNil(user)} variables={{userID: R.prop('userID', user)}}>
     {
-        ({data: {user}, error, loading}) => (
-            !R.isNil(user) &&
-            R.compose(
-                R.map(
-                    ({objectName, filename}) => (
-                        <Segment clearing key={objectName}>
-                            {filename}
-                            <Button style={{background:'#82E0AA', color:'#ffff'}} floated='right' content='Download'
-                                as='a'
-                                // target="_blank"
-                                download
-                                href={`http://localhost:4000/upload/download/${objectName}`}
-                            />
-                        </Segment>
-                    )
-                ),            
-                R.prop('uploads')
-            )(user)
-        )
+        ({data: {user}, error, loading, refetch}) => (
+                <Segment basic>
+                <Button inverted color='green' as={'label'} content='REFRESH' fluid  onClick={() => refetch()}/>
+                {
+                    !R.isNil(user) &&
+                    R.compose(
+                        R.map(
+                            ({bucketName, objectName, filename}) => (
+                                <Segment clearing key={objectName}>
+                                    {filename}
+                                    <DownloadButton bucketName={bucketName} objectName={objectName} filename={filename} />
+                                </Segment>
+                            )
+                        ),            
+                        R.prop('uploads')
+                    )(user)
+                }
+                </Segment>
+            )
     }
     </Query>
 )
 
 const App = ({setPageKey, user, result, createUpload}) => {
     const [files, setFiles] = useState([])
-    // useEffect(
-    //      () => {
-    //         const fetchUploads = async () => {
-    //             const uploads = await fetch('/upload/all')
-    //             const data = await uploads.json()
-    //             setFiles(data)
-    //         }
-    //         fetchUploads()
-    //     }, 
-    //     []
-    // )
     const {userID} = user
     return (
         <div style={{background: '#82E0AA'}}>
         <Button
-            size='medium' inverted floated='right'
+            size='massive'  style={{background:'#82E0AA', color:'#ffff'}} as={'label'} floated='right'
             animated='fade'
             onClick={() => setPageKey('homePage')}
         >
             <Button.Content content='LOG-OUT' hidden/>
             <Button.Content content={`${R.prop('username', user)}... do you want to leave?`} visible/>
         </Button>
-        <Segment  attached='bottom' size='massive' placeholder>
-            <Grid columns={2} style={{ height: '90vh' }} stackable textAlign='center'>
-                <Divider hidden vertical centered='true'>
-                {/* {<Button type ='submit' onClick={handleClick} size='massive' animated='fade' style={{background:'#82E0AA', color:'#ffff'}} as={'label'} htmlFor={'upload'}>
-                    <Button.Content visible>
-                        CLICK TO UPLOAD
-                    </Button.Content>
+        {/* <Divider hidden vertical centered='true'> */}
+                {<Button size='massive' animated='fade' style={{background:'#82E0AA', color:'#ffff'}} as={'label'}>
                     <Button.Content hidden>
-                        <Icon name='upload' />
+                        <Dropzone onDrop={(files)=> createUpload({variables: {userID, file: R.head(files)}})}>
+                        {({getRootProps, getInputProps}) => (
+                            <div {...getRootProps()}>
+                            <Icon name='upload'></Icon>
+                            <input {...getInputProps()} />
+                            </div>
+                        )}
+                        </Dropzone>
                     </Button.Content>
-                </Button>} */}
-                <Dropzone onDrop={(files)=> createUpload({variables: {userID, file: R.head(files)}})}>
-                {({getRootProps, getInputProps}) => (
-                    <div {...getRootProps()}>
-                    <p>Drop files here, or click to select files</p>
-                    <input {...getInputProps()} />
-                    </div>
-                )}
-                </Dropzone>
-                </Divider>
+                    <Button.Content visible>
+                            CLICK TO UPLOAD
+                    </Button.Content>
+                </Button>}
+        {/* </Divider> */}
+        <Segment>
+            <Grid columns={3} style={{ height: '200vh' }} stackable textAlign='center'>
                 <Grid.Row verticalAlign='top'>
                     <Grid.Column>
                         <Header style={{color:'#82E0AA'}} icon>
                             <br></br>
-                            <Icon style={{color:'#82E0AA'}} name='circle outline' />
-                            <br></br>
-                            NUMBER OF UPLOADED FILES
-                            <br></br><br></br>
-                            {R.length(files)}
-                        </Header>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Header style={{color:'#82E0AA'}} icon>
-                            <br></br>
-                            <Icon style={{color:'#82E0AA'}} name='circle' />
+                            <Icon style={{color:'#82E0AA'}} name='file alternate outline' />
                             <br></br>
                             LIST OF UPLOADED FILES
                             <UploadsList user={user} />
-                            {/* {R.map(
-                                ({objectName, filename}) => (
-                                    <Segment clearing key={objectName}>
-                                        {filename}
-                                        <Button style={{background:'#82E0AA', color:'#ffff'}} floated='right' content='Download'
-                                            as='a'
-                                            target="_blank"
-                                            download
-                                            href={`http://localhost:4000/upload/download/${objectName}`}
-                                        />
-                                    </Segment>
-                                ),
-                                files
-                            )} */}
                         </Header>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
-            {/* <input hidden id={'upload'} type={'file'} 
-                onChange={(event) => {
-                    const file = R.compose(R.head, R.path(['target', 'files']))(event)
-                    // Send file to minio
-                //    const xhr = new XMLHttpRequest ()
-                //    xhr.open('PUT', 'http://localhost:4000/upload', true)
-                //    xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
-                // //    xhr.withCredentials = true
-                //    const formData = new FormData()
-                //    formData.append('uploadedFile', file)
-                //    xhr.send(formData)
-                   xhr.onload = () => {
-                    //    if (xhr.status == 200) {
-
-                    //    }
-                   }
-                   //console.log(event.target.files)
-                }} */}
         </Segment>
         </div>
         )
