2960f0ed132eb64fd834952938e894584f8374c7
Micaela <micaela@rt3347ml-ccm.sickkids.ca>
2019-06-07 10:51:13 -0400

    remove commented methods

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const multer = require('multer')
-//const minio = require('minio')
 const { ApolloServer, ApolloError} = require('apollo-server');
 const {Upload, User, db} = require('./mongoSchema');
 const {minioClient} = require('./minioClient')
@@ -8,8 +7,6 @@ const {resolvers} = require('./resolvers')
 const {typeDefs} = require('./typeDefs');
 const app = express()
 const port = 4000
-const upload = multer({dest: '/Users/micaela/Desktop/my-app/server/tmp'})
-const R= require('ramda')
 
 const context = async ({req}) =>{
   return{
@@ -52,24 +49,6 @@ objectsStream.on('error', function(e) {
   console.log(e)
 })
 
-
-
-// const fileNotUploaded = (file, allUploads) => (
-//     R.not(R.includes(
-//         R.prop('originalname', file),
-//         R.pluck('filename', allUploads)
-//     ))
-// )
-
-// app.get('/upload/all',
-//     async (req, res) => {
-//         const uploads = await Upload.find({})
-//         // console.log(uploads)
-//         res.json(uploads)
-//         //res.sendStatus(200)
-//     }
-// )
-
 app.get('/upload/download/:objectName',
     async (req, res, next) => {
         const {params: {objectName}} = req
@@ -87,32 +66,6 @@ app.get('/upload/download/:objectName',
         })
 })
 
-
-// app.put('/upload', upload.single('uploadedFile'),
-//     async (req, res, next) => {
-//         const {file} = req
-//         const {originalname} = file;
-//         const upload = new Upload({bucketName: '5cf69f0c3d57a51cba9aece3', filename: originalname})
-//         const allUploads = await Upload.find({})
-//         // fileNotUploaded needs to be refactored to account for the userID bucketName
-//         if (fileNotUploaded(file, allUploads)) {
-//             const {bucketName, objectName, filename} = await upload.save()
-//             console.log('UPLOADING FILE', bucketName, objectName)
-//             const metaData = { 
-//                 'Content-Type': 'application/octet-stream',
-//                 'X-Amz-Meta-Testing': 1234,
-//                 'example': 5678
-//             }
-//             console.log(file.path, 'filePath')
-//             // Using fPutObject API upload your file to the bucket
-//             minioClient.fPutObject(`${bucketName}`, `${objectName}`, file.path, metaData, function(err, etag) {
-//                 if (err) return console.log(err, etag)
-//                 console.log('File uploaded successfully.')
-//             })
-//         }
-//         res.sendStatus(200);
-//     }
-// )
 db.once(
     'open',
     () => {
