e6d32de445f78f979f38eb3a9bdea10084cf2493
Micaela <micaela@rt3347ml-ccm.sickkids.ca>
2019-06-07 10:52:35 -0400

    remove commented methods

--- src/index.js
+++ src/index.js
@@ -7,18 +7,12 @@ import App from './App';
 import './index.css';
 import { ApolloProvider, Query,} from "react-apollo";
 import { ApolloClient } from 'apollo-client';
-import gql from 'graphql-tag';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
 import {createUploadLink} from 'apollo-upload-client'
 import * as R from 'ramda'
 
 const cache = new InMemoryCache();
-// const link = new HttpLink({
-//   uri: 'http://localhost:8000/'
-// })
+
 
 const link = createUploadLink({uri:'http://localhost:8000'})
 
@@ -26,19 +20,6 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
   link
 })
-
-// const USER_QUERY = gql`
-//   {
-//     uploads (userID:"5ced58bc6ac1cfab2a84b89a"){
-//       filename
-//     }
-//   }
-// `;
-
-// //Running our query outside of React
-// client.query({
-//   query: USER_QUERY
-// }).then(res => console.log(res))
 
 const Router = () => {
   const [user, setUser] = useState(null)
